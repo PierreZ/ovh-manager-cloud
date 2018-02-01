49d780e6a065bce615e93f28dc45964f513dbddf
Mathieu <mathieu.boulianne@corp.ovh.com>
2018-02-01 15:29:45 -0500

    clean up

--- client/app/iplb/iplb-detail.controller.js
+++ client/app/iplb/iplb-detail.controller.js
@@ -1,12 +1,22 @@
 class IpLoadBalancerDetailCtrl {
-    constructor ($stateParams, CloudMessage, IpLoadBalancerConfigurationService) {
+    constructor ($stateParams, CloudMessage, CloudNavigation, IpLoadBalancerConfigurationService) {
         this.$stateParams = $stateParams;
         this.CloudMessage = CloudMessage;
+        this.CloudNavigation = CloudNavigation;
         this.IpLoadBalancerConfigurationService = IpLoadBalancerConfigurationService;
         this.messages = [];
+
+        this.serviceName = $stateParams.serviceName;
     }
 
     $onInit () {
+        this.CloudNavigation.init({
+            state: "network.iplb.detail",
+            stateParams: {
+                serviceName: this.serviceName
+            }
+        });
+
         this.CloudMessage.unSubscribe("network.iplb.detail");
         this.messageHandler = this.CloudMessage.subscribe("network.iplb.detail", { onMessage: () => this.refreshMessage() });
         this.checkPendingChanges();
@@ -18,7 +28,7 @@ class IpLoadBalancerDetailCtrl {
 
     checkPendingChanges () {
         this.IpLoadBalancerConfigurationService.getPendingChanges(
-            this.$stateParams.serviceName
+            this.serviceName
         )
             .then(changes => _.chain(changes).map("number").sum().value() > 0)
             .then(hasChanges => {
